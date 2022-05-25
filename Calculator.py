1f1636212de50d87e01c53d8a81a831354c78152
Kulaya <106235512+Kulaya@users.noreply.github.com>
2022-05-25 12:36:18 +0300

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -6,6 +6,7 @@
 
 import streamlit as st
 st.title("Trapezium Calculator")
+st.header("By Aviti")
 base_1 = int(st.number_input("base 1 is:"))
 base_2 = int(st.number_input("base 2 is:"))
 height = int(st.number_input("height is:"))
